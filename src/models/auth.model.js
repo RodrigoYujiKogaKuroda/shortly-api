1322befef356b4c3f16599c3e1ce483690512503
RodrigoYujiKogaKuroda <yujikuroda.rykk@gmail.com>
2022-12-21 10:58:04 -0300

    feat: Added auth models

--- src/models/auth.model.js
+++ src/models/auth.model.js
@@ -1,7 +1,13 @@
 import Joi from "joi";
 
 export const signUpModel = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    confirmPassword: joi.string().valid(joi.ref('password'))
 });
 
 export const signInModel = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
 });

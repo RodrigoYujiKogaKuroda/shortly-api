2c45316af817e3b1403816ecc2398052088e6961
RodrigoYujiKogaKuroda <yujikuroda.rykk@gmail.com>
2022-12-21 16:27:58 -0300

    feat: Added auth middlewares

--- src/middlewares/authModelValidation.middleware.js
+++ src/middlewares/authModelValidation.middleware.js
@@ -3,8 +3,34 @@ import { signUpModel, signInModel } from "../models/auth.model.js";
 
 export async function signUpModelValidation(req, res, next) {
 
-    try {
+    const { 
+        name,
+        email,
+        password,
+        confirmPassword
+    } = req.body;
 
+    const user = {
+        name,
+        email,
+        password,
+        confirmPassword
+    };
+
+    const { error } = signUpModel.validate(user, { abortEarly: false });
+    if (error) {
+        const errors = error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try {
+        const users = await connection.query(
+            "SELECT email FROM users WHERE email=$1;",
+            [email]
+        );
+        if (users.rows[0]) {
+            return res.sendStatus(409);
+        }
     } catch (err) {
         res.status(500).send(err.message);
     }
@@ -16,7 +42,21 @@ export async function signUpModelValidation(req, res, next) {
 export async function signInModelValidation(req, res, next) {
 
     try {
-
+        const {
+            email,
+            password
+        } = req.body;
+    
+        const user = {
+            email,
+            password
+        };
+    
+        const { error } = signInModel.validate(user, { abortEarly: false });
+        if (error) {
+            const errors = error.details.map((detail) => detail.message);
+            return res.status(422).send(errors);
+        }
     } catch (err) {
         res.status(500).send(err.message);
     }

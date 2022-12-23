7b26e6a194cfbb3e8776f99e73ecbabced8da5d5
RodrigoYujiKogaKuroda <yujikuroda.rykk@gmail.com>
2022-12-23 02:03:16 -0300

    feat: Added user repository

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,9 +1,28 @@
 import { usersRepository } from "../repositories/users.repository.js";
+import { sessionRepository } from "../repositories/sessions.repository.js";
 
 export async function getUser (req, res) {
 
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+    if(!token){
+        return res.sendStatus(401);
+    }
+
     try {
-        
+        const session = await sessionRepository.connectSession(token);
+        if (!session.rows[0]) {
+            return res.sendStatus(401);
+        }
+
+        const user = await sessionRepository.findUser(session.rows[0].user_id);
+        if (!user.rows[0]) {
+            return res.sendStatus(404);
+        }
+
+        delete user.rows[0].password;
+        const list = await usersRepository.getUser(user.rows[0].id);
+        res.status(200).send(list);
     } catch (err) {
         res.status(500).send(err.message);
     }
--- src/repositories/users.repository.js
+++ src/repositories/users.repository.js
@@ -1,7 +1,28 @@
 import { connection } from "../database/database.js";
 
-async function getUser() {
-	return connection.query();
+async function getUser(id) {
+	return connection.query(
+        `SELECT 
+            u.id,
+            u.name,
+            SUM(l.visit_count) as "visitCount"
+        FROM links l
+        JOIN users u
+            ON l.user_id = u.id
+        WHERE l.user_id = $1
+        GROUP BY u.id;
+        SELECT row_to_json(l)
+        FROM (
+            SELECT
+                l.id,
+                l.short_url as "shortUrl",
+                l.url,
+                l.visit_count as "visitCount"
+            FROM links l
+            WHERE l.user_id = $1
+        ) as "shortenedUrls"`,
+        [id]
+    );
 }
 
 export const usersRepository = {

f4e4a69fcb2085756b569a0d4f9481a37547c3ce
RodrigoYujiKogaKuroda <yujikuroda.rykk@gmail.com>
2022-12-23 17:12:03 -0300

    feat: Added get in rankings controller

--- src/controllers/rankings.controller.js
+++ src/controllers/rankings.controller.js
@@ -3,7 +3,8 @@ import { rankingsRepository } from "../repositories/rankings.repository.js";
 export async function getRanking (req, res) {
 
     try {
-        
+       const ranking = await rankingsRepository.getRanking();
+       res.status(200).send(ranking.rows[0].json_agg);
     } catch (err) {
         res.status(500).send(err.message);
     }
--- src/repositories/rankings.repository.js
+++ src/repositories/rankings.repository.js
@@ -1,7 +1,23 @@
 import { connection } from "../database/database.js";
 
 async function getRanking() {
-	return connection.query();
+	return connection.query(`
+        WITH full_info AS (
+            SELECT
+                u.id,
+                u.name,
+                COUNT(l.user_id) as "linksCount",
+                SUM(l.visit_count) as "visitCount"
+            FROM links l
+            LEFT JOIN users u
+                ON l.user_id = u.id
+            GROUP BY u.id
+            ORDER BY "visitCount"
+            LIMIT 10
+        )
+        SELECT JSON_AGG(full_info.*)
+        FROM full_info;
+    `);
 }
 
 export const rankingsRepository = {
